6a58423c23871bbd5780dc2bd4127b90f787206a
mh <maodeai@126.com>
2020-07-07 17:36:55 +0800

    add first

---
+++ flink-study/cn/com/flink/TalbeAPI.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/7/7 17:35
+# @Author : mh
+# @Site : 
+# @File : TalbeAPI.py
+# @Software: PyCharm
---
+++ flink-study/cn/com/flink/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/7/7 17:34
+# @Author : mh
+# @Site : 
+# @File : __init__.py.py
+# @Software: PyCharm
