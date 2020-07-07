cda7c66e70c31d2efb3281e12748120d2bb706ce
mh <maodeai@126.com>
2020-07-07 18:52:00 +0800

    update TalbeApi

--- flink-study/cn/com/flink/TalbeAPI.py
+++ flink-study/cn/com/flink/TalbeAPI.py
@@ -5,4 +5,20 @@
 # @File : TalbeAPI.py
 # @Software: PyCharm
 
+from pyflink.dataset import ExecutionEnvironment
+from pyflink.table import TableConfig, DataTypes, BatchTableEnvironment
+from pyflink.table.descriptors import Schema, OldCsv, FileSystem
+
+exec_env = ExecutionEnvironment.get_execution_environment()
+exec_env.set_parallelism(3)
+t_config = TableConfig()
+t_evn = BatchTableEnvironment.create(exec_env, t_config)
+
+t_evn.connect(FileSystem().path('/tmp/input')).with_format(OldCsv.field('word'),DataTypes.STRING()) \
+    .with_schema(Schema().field('word'),DataTypes.STRING()) \
+    .create_temporary_table('mySource')
+
+t_evn.connect(FileSystem.path('/tmp/output')).with_format(OldCsv.field_delimiter('\t') \
+                                                          .field('word',DataTypes.STRING() \
+                                                           .with_schema()
 
